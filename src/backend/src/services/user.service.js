e2303f1c93e23723f44882edff90ea300347e900
gisellenrosa <gisellenrosa@gmail.com>
2023-10-04 20:52:37 -0300

    fix:: add env example and readMe

--- src/backend/src/services/user.service.js
+++ src/backend/src/services/user.service.js
@@ -1,34 +1,34 @@
 import { prisma } from "../database/prisma.provider.js";
 
 class UserService {
-  async findUsers() {
-    return await prisma.user.findMany();
-  }
+	async findUsers() {
+		return await prisma.users.findMany();
+	}
 
-  async findById(id) {
-    return await prisma.user.findUniqueOrThrow({
-      where: { id },
-    });
-  }
+	async findById(id) {
+		return await prisma.users.findUniqueOrThrow({
+			where: { id },
+		});
+	}
 
-  async create(payload) {
-    return await prisma.user.create({
-      data: payload,
-    });
-  }
+	async create(payload) {
+		return await prisma.users.create({
+			data: payload,
+		});
+	}
 
-  async update(id, payload) {
-    return await prisma.user.update({
-      where: { id },
-      data: payload,
-    });
-  }
+	async update(id, payload) {
+		return await prisma.users.update({
+			where: { id },
+			data: payload,
+		});
+	}
 
-  async delete(id) {
-    return await prisma.user.delete({
-      where: { id },
-    });
-  }
+	async delete(id) {
+		return await prisma.users.delete({
+			where: { id },
+		});
+	}
 }
 
 export default new UserService();
