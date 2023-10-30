391449d26c7d109443b89530856bf1944a76e788
gisellenrosa <gisellenrosa@gmail.com>
2023-10-29 22:29:40 -0300

    fix

--- src/frontend/src/components/Input/style.js
+++ src/frontend/src/components/Input/style.js
@@ -8,3 +8,11 @@ export const Input = styled.input`
     
     color: #1C1C1C;
 `;
+export const Container = styled.input`
+border: 1px solid #F39D54;
+border-radius: 10px;
+width: 100%;
+padding-left: 7px;
+
+color: #1C1C1C;
+`;
