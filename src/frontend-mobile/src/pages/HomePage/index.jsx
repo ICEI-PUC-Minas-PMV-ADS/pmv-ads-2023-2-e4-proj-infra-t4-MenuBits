2ddd8bb6421a362a028c6a60464fd93d75837234
gisellenrosa <gisellenrosa@gmail.com>
2023-11-18 20:13:34 -0300

    fix errors

--- src/frontend-mobile/src/pages/HomePage/index.jsx
+++ src/frontend-mobile/src/pages/HomePage/index.jsx
@@ -1,15 +1,12 @@
+import { Text } from "react-native";
+import { Container } from "./styles";
 
 const Home = () => {
-
-	return (
-		<Container>
-		
-        TA RODANDO HEIN!
-	
-		</Container>
-	);  
+  return (
+    <Container>
+      <Text> TA RODANDO HEIN!</Text>
+    </Container>
+  );
 };
 
 export default Home;
-
-
