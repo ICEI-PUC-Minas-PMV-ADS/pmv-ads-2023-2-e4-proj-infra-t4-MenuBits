f845f9dc184426b4ec0df81c9a890a5ae21cc65d
Philipe Rocha <philiperochad.js@gmail.com>
2023-11-24 21:13:07 -0300

    alterações diversas + menuEditorPage

---
+++ src/frontend-mobile/src/components/Buttons.jsx
@@ -0,0 +1,24 @@
+import styled from "styled-components/native";
+
+export const Button = styled.Pressable`
+  color: ${(props) => (props.$variant == "secondary" ? "#000" : "#fff")};
+  background-color: ${(props) => (props.$variant == "secondary" ? "#fff" : "#C60404")};
+  border: ${(props) => (props.$variant == "secondary" ? "2px solid #C75B00" : "none")};
+  border-radius: ${(props) => (props.$rounded == true ? "40px" : "10px")};
+  font-size: ${(props) => (props.$size == 'lg' ? "1rem" : "1rem")};
+  padding: ${(props) => (props.$size == 'lg' ? "20px 40px" : "10px 20px")};
+  transition: all 0.2s ease-in-out;
+  font-weight: 500;
+
+  &:hover {
+    color: #fff;
+    background-color: ${(props) => (props.$variant == "secondary" ? "#C75B00" : "#c60404b6")};
+    transform: scale(1.025);
+  }
+
+  &:active {
+    color: #fff;
+    background-color: #b70808;
+    transform: scale(0.95);
+  }
+`;
--- src/frontend-mobile/src/routes/app.routes.jsx
+++ src/frontend-mobile/src/routes/app.routes.jsx
@@ -1,5 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomePage from "../pages/HomePage";
+import MenuEditor from "../pages/MenuEditor";
+//import MenuPage from "../pages/MenuPage";
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -10,7 +12,9 @@ export function AppRoutes() {
 				headerShown: false,
 			}}
 		>
-			<Screen name="HomePage" component={HomePage} />
+			{/* <Screen name="HomePage" component={HomePage} /> */}
+			<Screen name="MenuEditor" component={MenuEditor} /> 
+			{/* <Screen name="MenuPage" component={MenuPage} /> */}
 		</Navigator>
 	);
 }
