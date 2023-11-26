5d836ac2161c875234984cb0d928ba2043fbdd4a
gisellenrosa <gisellenrosa@gmail.com>
2023-11-26 00:00:52 -0300

    add scrollview to page

--- src/frontend-mobile/src/pages/MenuPage/styles.js
+++ src/frontend-mobile/src/pages/MenuPage/styles.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Container = styled.View`
+export const Container = styled.ScrollView`
   background-size: cover;
   width: 100%;
   height: 100vh;
