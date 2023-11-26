189917e629cf376123106841f4003404bda09107
Philipe Rocha <philiperochad.js@gmail.com>
2023-11-26 20:49:38 -0300

    integracao-parcial

--- src/frontend-mobile/src/pages/QrCode/index.jsx
+++ src/frontend-mobile/src/pages/QrCode/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
+import { Button } from '../../components/Buttons';
 
 const QRCodeScanner = (props) => {
   const [hasPermission, setHasPermission] = useState(null);
