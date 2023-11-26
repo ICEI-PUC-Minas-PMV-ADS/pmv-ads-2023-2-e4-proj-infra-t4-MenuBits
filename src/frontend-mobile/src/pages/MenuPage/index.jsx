25060ff785014f047ab675d4c7ef3e78f487fc00
gisellenrosa <gisellenrosa@gmail.com>
2023-11-26 14:51:09 -0300

    Add menuPage to handleClick to add cart

--- src/frontend-mobile/src/pages/MenuPage/index.jsx
+++ src/frontend-mobile/src/pages/MenuPage/index.jsx
@@ -14,12 +14,32 @@ import {
 } from "./styles.js";
 import CardItem from "../../components/CardItem";
 import { useMenuBitsState } from "../../context/MenuBitsContext";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 export default function MenuPage() {
   const [menuData, setMenuData] = useState();
   const { menuId, restaurantData } = useMenuBitsState();
+  const [selectedOrder, setSelectedOrder] = useState([]);
 
   const navigation = useNavigation();
+
+ const handleClick = useCallback(async (item) => {
+  try {
+    console.warn("PASSOU AQUI", selectedOrder);
+    const selectedOrderUpdated = [...selectedOrder, item];
+    console.warn("PASSOU AQUI 2", selectedOrderUpdated);
+    setSelectedOrder(selectedOrderUpdated);
+    
+    await AsyncStorage.setItem('pedidos', JSON.stringify(selectedOrderUpdated));
+    
+    alert('Adicionado Com sucesso!');
+  } catch (error) {
+    console.error('Erro ao adicionar o pedido:', error);
+    console.warn('Pedido não adicionado, tente novamente')
+  }
+}, [selectedOrder]);
+
 
   useEffect(() => {
     axios
