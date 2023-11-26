a5efa95b3eb5da825881e1362591430b1ed61ab9
gisellenrosa <gisellenrosa@gmail.com>
2023-11-26 00:01:42 -0300

    remove the height componetn

--- src/frontend-mobile/src/pages/MenuPage/styles.js
+++ src/frontend-mobile/src/pages/MenuPage/styles.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 export const Container = styled.ScrollView`
   background-size: cover;
   width: 100%;
-  height: 100vh;
   display: flex;
   flex-direction: column;
   gap: 10px;
