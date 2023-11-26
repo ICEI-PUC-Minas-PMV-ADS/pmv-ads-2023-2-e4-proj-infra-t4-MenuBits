7e6fc23db49f2c8e32cb7adaae330b5afcd55191
gisellenrosa <gisellenrosa@gmail.com>
2023-11-26 18:06:06 -0300

    fix data

--- src/frontend-mobile/src/routes/index.jsx
+++ src/frontend-mobile/src/routes/index.jsx
@@ -1,5 +1,6 @@
 import {AppRoutes} from "./app.routes";
 import HeaderSidebar from './app.sidebar';
+import { NavigationContainer } from "@react-navigation/native";
 
 export function Routes() {
   return (
