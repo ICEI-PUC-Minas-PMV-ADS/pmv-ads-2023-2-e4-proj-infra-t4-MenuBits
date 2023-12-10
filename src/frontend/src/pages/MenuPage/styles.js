ebd6b18effba56ff704df2c040a610f9e6638941
gisellenrosa <gisellenrosa@gmail.com>
2023-12-10 02:11:15 -0300

    add template style

--- src/frontend/src/pages/MenuPage/styles.js
+++ src/frontend/src/pages/MenuPage/styles.js
@@ -10,12 +10,12 @@ export const Container = styled.div`
 `;
 
 export const Banner = styled.div`
-  background-image: url("src/assets/pizzaNostra.png");
+  width: 100%; /* Defina largura e altura adequadas */
+  height: 400px;
+  background-image: url("https://images.unsplash.com/photo-1464093515883-ec948246accb?q=80&w=2059&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
   background-position: center;
   background-size: cover;
   background-repeat: no-repeat;
-  width: 100%;
-  height: 25vh;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -23,9 +23,9 @@ export const Banner = styled.div`
   grid-template-rows: 1fr 1fr 1fr;
 `;
 export const TextBanner = styled.h1`
-  color: white;
+  color: black;
   font-size: 40px;
-  text-shadow: 1px 1px 3px rgba(0, 0, 0, 0.5); /* Adiciona uma sombra para criar o efeito de relevo */
+  text-shadow: 3px 2px 5px rgba(255, 255, 255, 0.9); /* Adiciona uma sombra para criar o efeito de relevo */
   justify-content: center;
   align-items: center;
 `;
